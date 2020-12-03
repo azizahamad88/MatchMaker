6b10a7882094286b0c29be5839fd5ce646aef9c6
azizahamad88 <azizahamad2014@yahoo.com>
2020-12-03 12:07:51 +0530

    Initial Commit

---
+++ MatchMakerAPI/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MatchMakerAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatchMakerAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
